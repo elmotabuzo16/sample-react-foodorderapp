cf7adf6a520915cd1ba1cb60e41d11e961ac542b
Elmo <elmotabuzojr.ust@gmail.com>
2023-02-26 22:21:32 +0800

    adding shopping cart component and modal via react portal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Header from "./components/Layout/Header";
+import Header from './components/Layout/Header';
 import MealsSummary from './components/Meals/MealsSummary';
-import Meals
- from './components/Meals/Meals';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-      
     </React.Fragment>
   );
 }
